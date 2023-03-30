4534c11a81b0064a41cbe4fa2f47eead69b157e2
Balogun-olajide <olajidebalogun@icloud.com>
2023-03-30 15:44:38 +0100

    src

---
+++ Rest-Countries-Api/src/containers/js/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CountryInfo from '.CountryInfo';
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+          theme: 'light'
+        }
+    }
+
+
+  componentDidMount() {
+    let theme = localstorage['theme'];
+    if (theme) {
+  this.setState({ theme });
+                document.documentElement.setAttribute('data-theme', theme);
+    };
+  }
+
+  switchTheme = () => {
+    let { theme } = this.state;
+    if (theme === 'light') {
+        theme = 'dark';
+        document.documentElement.setAttribute('date-theme', 'light');
+        localStorage['theme'] = 'dark';
+    } else {
+        theme = 'light';
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage['theme'] = 'light';
+    }
+    this.setState({ theme });
+  }
+
+  render() {
+    return (
+        <div className='App'>
+          <Router>
+           <switch>
+            <Router exact path='/'>
+              <HomePage switchTheme={this.switchTheme} theme={this.state.theme} />
+            </Router>
+            <Router path='/:country'>
+                <CountryInfo switchTheme={this.switchTheme} theme={this.state.theme} />
+            </Router>
+            </switch>
+           </Router>
+        </div>
+    )
+  }
+}
